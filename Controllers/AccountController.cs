40ff00b1f08ecf0a23600070e655c074b4a64c72
maepocuser2 <maepocuser2@EC2AMAZ-UL0NSS6>
2020-10-23 10:38:40 +0000

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using AccountLibrary.API.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace AccountLibrary.API.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private IConfiguration _configuration;
+
+        public AccountController(
+            IMapper mapper, ILogger<AccountController> logger, IConfiguration iConfig)
+        {
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+
+            _logger = logger;
+            _configuration = iConfig;
+
+
+        }
+
+        [Route("account")]
+        [HttpGet]
+        public async Task<ActionResult> GetAccounts()
+        {
+            string token = string.Empty;
+            _logger.LogInformation("Method start account");
+            if (Request.Headers.ContainsKey("Authorization"))
+            {
+                token = Request.Headers["Authorization"];
+                string url = _configuration.GetSection("MySettings").GetSection("URL").Value;
+                var jwttoken = token.Split(" ")[1];
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                var jToken = (JwtSecurityToken)tokenHandler.ReadToken(jwttoken);
+                var custId = jToken.Claims.ToList();
+                var customerid = custId.Find(x => x.Type == "custom:CustomerId").Value;
+                _logger.LogInformation("customer id passed " + customerid);
+                System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+                client.DefaultRequestHeaders.Add("x-api-customerid", customerid);
+                _logger.LogInformation("http call start");
+                var accountsResponse = await client.GetAsync(url);
+                _logger.LogInformation("http call end");
+
+                var responseBodyAsText = await accountsResponse.Content.ReadAsStringAsync();
+                _logger.LogInformation("string response " + responseBodyAsText);
+
+                if (accountsResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    return Ok(JsonConvert.DeserializeObject(responseBodyAsText));
+
+            }
+            return NotFound();
+
+        }
+    }
+
+    }
---
+++ Models/AccountDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AccountLibrary.API.Models
+{
+    public class AccountDetails
+    {
+        public string AccountIdentifier { get; set; }
+        public string AccountType { get; set; }
+        public string AccountSubType { get; set; }
+        
+        public string AccountHolderName { get; set; }
+        public string AccountStatus { get; set; }
+        
+        public string AccountName { get; set; }
+        public string OpenDate { get; set; }
+        public string SortCode { get; set; }
+        public string ProductName { get; set; }
+        public double AvailableBalance { get; set; }
+        public double LedgerBalance { get; set; }
+
+    }
+
+}
